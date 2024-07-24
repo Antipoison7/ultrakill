b23efd40e8d784f661ebb205cb294ea725c57c26
Antipoison7 <orders.connor@gmail.com>
2024-07-25 01:47:44 +1000

    Removed old code

--- src/main/java/app/AdvancedTimes.java
+++ src/main/java/app/AdvancedTimes.java
@@ -43,44 +43,6 @@ public class AdvancedTimes implements Handler {
         // Add the body
         html = html + "<body>";
 
-        // Add the topnav
-        // This uses a Java v15+ Text Block
-        html = html + """
-            <div class='topnav'>
-                <a href='/'>Homepage</a>
-                <a href='mission.html'>Our Mission</a>
-                <a href='page2A.html'>Sub Task 2.A</a>
-                <a href='page2B.html'>Sub Task 2.B</a>
-                <a href='page3A.html'>Sub Task 3.A</a>
-                <a href='page3B.html'>Sub Task 3.B</a>
-            </div>
-        """;
-
-        // Add header content block
-        html = html + """
-            <div class='header'>
-                <h1>Subtask 2.B</h1>
-            </div>
-        """;
-
-        // Add Div for page Content
-        html = html + "<div class='content'>";
-
-        // Add HTML for the page content
-        html = html + """
-            <p>Subtask 2.B page content</p>
-            """;
-
-        // Close Content div
-        html = html + "</div>";
-
-        // Footer
-        html = html + """
-            <div class='footer'>
-                <p>COSC2803 - Studio Project Starter Code (Apr24)</p>
-            </div>
-        """;
-
         // Finish the HTML webpage
         html = html + "</body>" + "</html>";
         
