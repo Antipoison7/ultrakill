77f69dedfb47a75d1c7e9c13f1674acff032d155
S4096467 <s4096467@student.rmit.edu.au>
2024-08-06 15:56:52 +1000

    made helper class for number processing

---
+++ src/main/java/helper/NumberConversion.java
@@ -0,0 +1,41 @@
+package helper;
+
+import app.JDBCConnection;
+import app.LevelTemplate;
+
+public class NumberConversion
+{
+    public Double ToSeconds(String TimeCode)
+    {
+        Double generatedTime = 0.0;
+
+        Double minutes = Double.parseDouble(TimeCode.substring(1,2));
+        Double secondsMs = Double.parseDouble(TimeCode.substring(3,9));
+
+        generatedTime = (minutes * 60) + secondsMs;
+
+        return generatedTime;
+    }
+
+    public String ToDuration(String userSeconds)
+    {
+        Double seconds = Double.parseDouble(userSeconds);
+        String durationString = "";
+
+        String Minutes = Integer.toString((int)(seconds / 60));
+        String Seconds = Double.toString((double)(seconds % 60));
+
+        if(Minutes.length() == 1)
+        {
+            durationString = "0" + Minutes;
+        }
+        else
+        {
+            durationString = Minutes;
+        }
+
+        durationString = durationString + ":" + Seconds;
+
+        return durationString;
+    }
+}
