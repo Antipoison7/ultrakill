68e5aa1465a183d14e9214aebe8d2bc1c174b455
S4096467 <s4096467@student.rmit.edu.au>
2024-08-10 16:12:07 +1000

    Started AdvancedTimes Page / class

---
+++ src/main/java/app/AdvancedRun.java
@@ -0,0 +1,78 @@
+package app;
+
+/**
+ * Class represeting a Country from the Studio Project database
+ *
+ * @author Halil Ali, 2024. email: halil.ali@rmit.edu.au
+ */
+
+public class AdvancedRun {
+   
+   private int RunID;
+   private String UserProfile;
+   private String Name;
+   private String Time;
+   private String Difficulty;
+   private String DifficultyDescription;
+   private String Category;
+   private String Comment;
+   private String Video;
+   private String LevelCode;
+
+   /**
+    * Create a Country and set the fields
+    */
+   public AdvancedRun(int Runid, String userprofile, String name, String time, String difficulty, String difficultyDescription, String category, String comment, String video, String levelcode) {
+      this.RunID = Runid;
+      this.UserProfile = userprofile;
+      this.Name = name;
+      this.Time = time;
+      this.Difficulty = difficulty;
+      this.DifficultyDescription = difficultyDescription;
+      this.Category = category;
+      this.Comment = comment;
+      this.Video = video;
+      this.LevelCode = levelcode;
+   }
+
+   public int getRunID(){ 
+      return RunID; 
+   }
+   
+   public String getPfp() {
+      return UserProfile;
+   }
+
+   public String getName() {
+      return Name;
+   }
+
+   public String getTime() {
+      return Time;
+   }
+
+   public String getDifficulty() {
+      return Difficulty;
+   }
+   
+   public String getDifficultyDescription() {
+      return DifficultyDescription;
+   }
+
+   public String getCategory() {
+      return Category;
+   }
+
+   public String getComment() {
+      return Comment;
+   }
+
+   public String getVideo() {
+      return Video;
+   }
+
+   public String getLevelCode() {
+      return LevelCode;
+   }
+   
+}
--- src/main/java/app/AdvancedTimes.java
+++ src/main/java/app/AdvancedTimes.java
@@ -29,12 +29,15 @@ public class AdvancedTimes implements Handler {
 
     @Override
     public void handle(Context context) throws Exception {
+        JDBCConnection jdbc = new JDBCConnection();
+        LevelTemplate selectedLevel = jdbc.getLevel(context.queryParam("level"));
         // Create a simple HTML webpage in a String
         String html = "<html>";
 
+
         // Add some Head information
         html = html + "<head>" + 
-               "<title>Subtask 2.2</title>";
+               "<title>Advanced Times</title>";
 
         // Add some CSS (external file)
         html = html + "<link rel='stylesheet' type='text/css' href='common.css' />";
@@ -43,13 +46,157 @@ public class AdvancedTimes implements Handler {
         // Add the body
         html = html + "<body>";
 
+        html = html + """
+                <div class="flexBox" style="justify-content:space-between; margin-bottom:10px;">
+                <a href="./"><button type="button" class="button">Back</button></a>
+                <h1 class='ultrakillTitleText'>-- Level Select --</h1>
+                <div style="width:15vw;height:4vw;padding: 0vw 2vw;"></div>
+                </div>
+                """;
+        html = html + "<div class='runSelectContainer'>";
+
+        html = html + """
+                    <div class="dropdown">
+                        <button onclick="myFunction()" class="dropbtn">
+                                """;
+
+        System.out.println("contextPath= " + context.queryParam("level"));
+        if(context.queryParam("level").equals("all"))
+        {
+            html = html + "SELECT A LEVEL";
+        }
+        else
+        {
+            html = html + selectedLevel.getLevelCode() + ": " + selectedLevel.getLevelName();
+        }
+        // html = html + ;
+
+        html = html + """
+                        </button>
+                        <div id="myDropdown" class="dropdown-content">
+                        """;
+
+        html = html + dropdownHandler();
+
+        html = html + """
+                        </div>
+                    </div> 
+                """;
+        html = html + "<div class='flexBox'style='margin-bottom:5px; margin-top: 5px; justify-content: space-around;'>";
+        html = html + "<button class='categoryButtonSelected' id='anyButton' onclick='toAny()'>Any %</button>";
+        html = html + "<button class='categoryButton' id='pButton' onclick='toP()'>P %</button>";
+        html = html + "<button class='categoryButton' id='noMoButton' onclick='toNoMo()'>NoMo</button>";
+        html = html + "</div>";
+        html = html + "</div>";
+
+        html = html + "<div class='runTableContainer'>";
+        html = html + "<table>";
+        if(context.queryParam("level").equals("all"))
+        {
+            html = html + """
+                <tr>
+                    <th>Rank</th>
+                    <th>Pfp</th>
+                    <th>Player</th>
+                    <th>Time</th>
+                    <th>Difficulty</th>
+                    <th>Inbounds</th>
+                    <th>Comment</th>
+                    <th>Video</th>
+                    <th>Level</th>
+                </tr>""";
+        }
+        else
+        {
+            html = html + """
+                <tr>
+                    <th>Rank</th>
+                    <th>Pfp</th>
+                    <th>Player</th>
+                    <th>Time</th>
+                    <th>Difficulty</th>
+                    <th>Inbounds</th>
+                    <th>Comment</th>
+                    <th>Video</th>
+                </tr>""";
+        }
+
+        html = html + "</table>";
+        
+        html = html + "</div>";
+
         // Finish the HTML webpage
-        html = html + "</body>" + "</html>";
+        html = html + "</body>";
+        
+        html = html + """
+                <script>
+                /* When the user clicks on the button,
+                    toggle between hiding and showing the dropdown content */
+                    function myFunction() 
+                    {
+                    document.getElementById("myDropdown").classList.toggle("show");
+                    }
+
+                    // Close the dropdown menu if the user clicks outside of it
+                    window.onclick = function(event) 
+                    {
+                        if (!event.target.matches('.dropbtn')) 
+                        {
+                            var dropdowns = document.getElementsByClassName("dropdown-content");
+                            var i;
+                            for (i = 0; i < dropdowns.length; i++) 
+                            {
+                                var openDropdown = dropdowns[i];
+                                if (openDropdown.classList.contains('show')) 
+                                {
+                                    openDropdown.classList.remove('show');
+                                }
+                            }
+                        }
+                    } 
+
+                    function toAny(){
+                        document.getElementById("anyButton").className = "categoryButtonSelected";
+                        document.getElementById("pButton").className = "categoryButton";
+                        document.getElementById("noMoButton").className = "categoryButton";
+                    }
+
+                    function toP(){
+                        document.getElementById("anyButton").className = "categoryButton";
+                        document.getElementById("pButton").className = "categoryButtonSelected";
+                        document.getElementById("noMoButton").className = "categoryButton";
+                    }
+
+                    function toNoMo(){
+                        document.getElementById("anyButton").className = "categoryButton";
+                        document.getElementById("pButton").className = "categoryButton";
+                        document.getElementById("noMoButton").className = "categoryButtonSelected";
+                    }
+                </script>
+                """;
+
+        html = html + "</html>";
         
 
         // DO NOT MODIFY THIS
         // Makes Javalin render the webpage
         context.html(html);
+    }
+
+    public String dropdownHandler()
+    {
+        String dropdownGenerator = "<a href='advTimes.html?level=all'>All</a>";
+
+        JDBCConnection jdbc = new JDBCConnection();
+
+        ArrayList <LevelTemplate> levels = jdbc.getAllLevels();
+
+        for(LevelTemplate thisLevel:levels)
+        {
+            dropdownGenerator = dropdownGenerator + "<a href='advTimes.html?level=" + thisLevel.getLevelCode() + "'>" + thisLevel.getLevelCode() + ": " + thisLevel.getLevelName() + "</a>";
+        }
+
+        return dropdownGenerator;
     }
 
 }
