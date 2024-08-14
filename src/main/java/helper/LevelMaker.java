2b7ed11ce83adb9da8ab5ce5cfbeb5b06b4d965f
S4096467 <s4096467@student.rmit.edu.au>
2024-08-14 11:35:50 +1000

    Allowed levelMaker to view context
    Gave levelMaker the ability to view GET and POST
    requests as well as removing the all function
    temporarily while being remade

--- src/main/java/helper/LevelMaker.java
+++ src/main/java/helper/LevelMaker.java
@@ -4,9 +4,13 @@ import app.AdvancedRun;
 import app.JDBCConnection;
 import app.LevelTemplate;
 
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
 import java.util.ArrayList;
 import java.util.Objects;
 
+//Makes the level blocks for ViewTimes / Simple Runs View
 public class LevelMaker
 {
     public String GetLevel(String levelCode)
@@ -107,32 +111,34 @@ public class LevelMaker
 
         for (AdvancedRun userRun : aRun)
         {
-            generatedString = generatedString + "<div class='runTableContainerInstance'>";
-                generatedString = generatedString + "<div><div></div><div></div><div></div>" + counter + "</div>";
-                generatedString = generatedString + "<div><img src='" + userRun.getPfp() + "'></div>";
-                generatedString = generatedString + "<div>" + userRun.getName() + "</div>";
-                generatedString = generatedString + "<div>" + numbs.ToDuration(userRun.getTime()) + "</div>";
-                generatedString = generatedString + "<div>" + userRun.getDifficulty() + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getCategory().equals("Any% OOB")||userRun.getCategory().equals("P% OOB")||userRun.getCategory().equals("NoMo OOB"))
-                    {
-                        generatedString = generatedString + "<img src='OutOfBounds.png' alt='Out Of Bounds'>";
-                    }
+            generatedString = generatedString + "<a href='/userRuns.html?runId=" + userRun.getRunID() + "&prev=All'>";
+                generatedString = generatedString + "<div class='runTableContainerInstance'>";
+                    generatedString = generatedString + "<div><div></div><div></div><div></div>" + counter + "</div>";
+                    generatedString = generatedString + "<div><img src='" + userRun.getPfp() + "'></div>";
+                    generatedString = generatedString + "<div>" + userRun.getName() + "</div>";
+                    generatedString = generatedString + "<div>" + numbs.ToDuration(userRun.getTime()) + "</div>";
+                    generatedString = generatedString + "<div>" + userRun.getDifficulty() + "</div>";
+                    generatedString = generatedString + "<div>";
+                        if(userRun.getCategory().equals("Any% OOB")||userRun.getCategory().equals("P% OOB")||userRun.getCategory().equals("NoMo OOB"))
+                        {
+                            generatedString = generatedString + "<img src='OutOfBounds.png' alt='Out Of Bounds'>";
+                        }
+                    generatedString = generatedString + "</div>";
+                    generatedString = generatedString + "<div>";
+                        if(userRun.getComment() != null)
+                        {
+                            generatedString = generatedString + "<img src='Comment.png' alt='Contains Comment'>";
+                        }
+                    generatedString = generatedString + "</div>";
+                    generatedString = generatedString + "<div>";
+                        if(userRun.getVideo() != null)
+                        {
+                            generatedString = generatedString + "<img src='Video.png' alt='Contains Video'>";
+                        }
+                    generatedString = generatedString + "</div>";
+                    generatedString = generatedString + "<div class='runTableContainerLevel'>" + userRun.getLevelCode() + "</div>";
                 generatedString = generatedString + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getComment() != null)
-                    {
-                        generatedString = generatedString + "<img src='Comment.png' alt='Contains Comment'>";
-                    }
-                generatedString = generatedString + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getVideo() != null)
-                    {
-                        generatedString = generatedString + "<img src='Video.png' alt='Contains Video'>";
-                    }
-                generatedString = generatedString + "</div>";
-                generatedString = generatedString + "<div class='runTableContainerLevel'>" + userRun.getLevelCode() + "</div>";
-            generatedString = generatedString + "</div>";
+            generatedString = generatedString + "</a>";
             counter = counter + 1;
         }
 
@@ -151,91 +157,37 @@ public class LevelMaker
 
         for (AdvancedRun userRun : aRun)
         {
-            generatedString = generatedString + "<div class='runTableContainerInstance'>";
-                generatedString = generatedString + "<div><div></div><div></div><div></div>" + counter + "</div>";
-                generatedString = generatedString + "<div><img src='" + userRun.getPfp() + "'></div>";
-                generatedString = generatedString + "<div>" + userRun.getName() + "</div>";
-                generatedString = generatedString + "<div>" + numbs.ToDuration(userRun.getTime()) + "</div>";
-                generatedString = generatedString + "<div>" + userRun.getDifficulty() + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getCategory().equals("Any% OOB")||userRun.getCategory().equals("P% OOB")||userRun.getCategory().equals("NoMo OOB"))
-                    {
-                        generatedString = generatedString + "<img src='OutOfBounds.png' alt='Out Of Bounds'>";
-                    }
-                generatedString = generatedString + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getComment() != null)
-                    {
-                        generatedString = generatedString + "<img src='Comment.png' alt='Contains Comment'>";
-                    }
-                generatedString = generatedString + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getVideo() != null)
-                    {
-                        generatedString = generatedString + "<img src='Video.png' alt='Contains Video'>";
-                    }
-                generatedString = generatedString + "</div>";
-                generatedString = generatedString + "</div>";
+            generatedString = generatedString + "<a href='/userRuns.html?runId=" + userRun.getRunID() + "&prev=" + level + "'>";
+                generatedString = generatedString + "<div class='runTableContainerInstance'>";
+                    generatedString = generatedString + "<div><div></div><div></div><div></div>" + counter + "</div>";
+                    generatedString = generatedString + "<div><img src='" + userRun.getPfp() + "'></div>";
+                    generatedString = generatedString + "<div>" + userRun.getName() + "</div>";
+                    generatedString = generatedString + "<div>" + numbs.ToDuration(userRun.getTime()) + "</div>";
+                    generatedString = generatedString + "<div>" + userRun.getDifficulty() + "</div>";
+                    generatedString = generatedString + "<div>";
+                        if(userRun.getCategory().equals("Any% OOB")||userRun.getCategory().equals("P% OOB")||userRun.getCategory().equals("NoMo OOB"))
+                        {
+                            generatedString = generatedString + "<img src='OutOfBounds.png' alt='Out Of Bounds'>";
+                        }
+                    generatedString = generatedString + "</div>";
+                    generatedString = generatedString + "<div>";
+                        if(userRun.getComment() != null)
+                        {
+                            generatedString = generatedString + "<img src='Comment.png' alt='Contains Comment'>";
+                        }
+                    generatedString = generatedString + "</div>";
+                    generatedString = generatedString + "<div>";
+                        if(userRun.getVideo() != null)
+                        {
+                            generatedString = generatedString + "<img src='Video.png' alt='Contains Video'>";
+                        }
+                    generatedString = generatedString + "</div>";
+                    generatedString = generatedString + "</div>";
+                generatedString = generatedString + "</a>";
             counter = counter + 1;
         }
 
         return generatedString;
     }
 
-    public String GetLevelRunsAll(String level)
-    {
-        String generatedString = "";
-        int counter = 1;
-
-        JDBCConnection jdbc = new JDBCConnection();
-        NumberConversion numbs = new NumberConversion();
-
-        ArrayList<AdvancedRun> aRun = jdbc.getComplexRunsAll(level);
-
-        for (AdvancedRun userRun : aRun)
-        {
-            generatedString = generatedString + "<div class='runTableContainerInstanceAny'>";
-                generatedString = generatedString + "<div><div></div><div></div><div></div>" + counter + "</div>";
-                generatedString = generatedString + "<div><img src='" + userRun.getPfp() + "'></div>";
-                generatedString = generatedString + "<div>" + userRun.getName() + "</div>";
-                generatedString = generatedString + "<div>" + numbs.ToDuration(userRun.getTime()) + "</div>";
-                generatedString = generatedString + "<div>" + userRun.getDifficulty() + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getCategory().substring(1,1).equals("A"))
-                    {
-                        generatedString = generatedString + "Any%";
-                    }
-                    else if(userRun.getCategory().substring(1,1).equals("P"))
-                    {
-                        generatedString = generatedString + "P%";
-                    }
-                    else if(userRun.getCategory().substring(1,1).equals("N"))
-                    {
-                        generatedString = generatedString + "NoMo";
-                    }
-                generatedString = generatedString + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getCategory().equals("Any% OOB")||userRun.getCategory().equals("P% OOB")||userRun.getCategory().equals("NoMo OOB"))
-                    {
-                        generatedString = generatedString + "<img src='OutOfBounds.png' alt='Out Of Bounds'>";
-                    }
-                generatedString = generatedString + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getComment() != null)
-                    {
-                        generatedString = generatedString + "<img src='Comment.png' alt='Contains Comment'>";
-                    }
-                generatedString = generatedString + "</div>";
-                generatedString = generatedString + "<div>";
-                    if(userRun.getVideo() != null)
-                    {
-                        generatedString = generatedString + "<img src='Video.png' alt='Contains Video'>";
-                    }
-                generatedString = generatedString + "</div>";
-                generatedString = generatedString + "</div>";
-            counter = counter + 1;
-        }
-
-        return generatedString;
-    }
 }
